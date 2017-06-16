b17a5729057bfb39374e970dda08d2e8aeefdf57
Michael <mvhenderson.4@icloud.com>
2017-06-16 12:50:36 -0500

    show student email in index.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -10,8 +10,8 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
-        public string EmailAddress { get; set; }
         public DateTime EnrollmentDate { get; set; }
+        public string EmailAddress { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
