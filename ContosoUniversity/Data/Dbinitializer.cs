78dce54cc1f97b3e921a5cd8597e93ac2e7934b7
Michael <mvhenderson.4@icloud.com>
2017-06-16 12:38:28 -0500

    add emails to dbinitializer

--- ContosoUniversity/Data/Dbinitializer.cs
+++ ContosoUniversity/Data/Dbinitializer.cs
@@ -18,15 +18,15 @@ namespace ContosoUniversity.Data
 
             var students = new Student[]
             {
-            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            new Student{FirstMidName="Michael",LastName="Henderson",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01"),EmailAddress="Carson.Alexander@contoso.edu"},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01"),EmailAddress="Meredith.Alonso@contoso.edu"},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01"),EmailAddress="Arturo.Anand@contoso.edu"},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01"),EmailAddress="Gytis.Barzdukas@contoso.edu"},
+            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01"),EmailAddress="Yan.Li@contoso.edu"},
+            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01"),EmailAddress="Peggy.Justice@contoso.edu"},
+            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01"),EmailAddress="Laura.Norman@contoso.edu"},
+            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01"),EmailAddress="Nino.Olivetto@contoso.edu"},
+            new Student{FirstMidName="Michael",LastName="Henderson",EnrollmentDate=DateTime.Parse("2005-09-01"),EmailAddress="Michael.Henderson@contoso.edu"}
 
             };
             foreach (Student s in students)
