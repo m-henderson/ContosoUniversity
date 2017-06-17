39c1799139e13c55b2a81aa83c7c8ba0cf6745e8
Michael <mvhenderson.4@icloud.com>
2017-06-17 17:24:01 -0500

    Add code that calls Include. ThenInclude, and AsNoTracking methods.

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -33,7 +33,13 @@ namespace ContosoUniversity.Controllers
                 return NotFound();
             }
 
+
+            // The Include and ThenInclude methods cause the context to load the Student.Enrollments navigation property, 
+            // and within each enrollment the Enrollment.Course navigation property.
             var student = await _context.Students
+                .Include(s=> s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
                 .SingleOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
