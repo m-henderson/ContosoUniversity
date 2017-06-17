6e71760dcd919faf0e8e15c78bf05c2750c5dd8e
Michael <mvhenderson.4@icloud.com>
2017-06-17 18:19:11 -0500

    bind email address field in student controller so it is saved in the database when new student is created.

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -60,7 +60,7 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("EmailAddress,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
             try
             {
